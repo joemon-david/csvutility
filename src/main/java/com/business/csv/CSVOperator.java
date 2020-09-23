164fa5dde9ee36d9bb3de6a2aa28e5667d3aa8b5
joemon-david <joemondev@gmail.com>
2020-09-23 22:02:48 +0530

    Fixed the issue with compare operation

--- src/main/java/com/business/csv/CSVOperator.java
+++ src/main/java/com/business/csv/CSVOperator.java
@@ -64,33 +64,37 @@ public class CSVOperator {
         }else
         {
             System.out.println("Files have different sizes. "+relativeInputFile1Path+" have "+(record1Size-1)+" rows , and "+relativeInputFile2Path+" have "+(record2Size-1));
-            recordList = (record1Size > record2Size)? recordList2:recordList1;
-            recordListNxt = (record1Size < record2Size)? recordList2:recordList1;
+            if(record1Size>record2Size)
+            {
+                recordList = recordList2;
+                recordListNxt = recordList1;
+            }else
+            {
+                recordList = recordList1;
+                recordListNxt = recordList2;
+            }
+
         }
 
-        List<CSVRecord> finalRecordListNxt = recordListNxt;
-        recordList.forEach((record1) ->
-        {
+//        List<CSVRecord> finalRecordListNxt = recordListNxt;
+        for (CSVRecord record1 : recordList) {
             long recordRowNumber = record1.getRecordNumber();
-            CSVRecord record2= finalRecordListNxt.get((int) recordRowNumber);
-                AtomicInteger record1ItemLoop= new AtomicInteger();
-                record1.forEach((item1) -> {
-                   int recordCurItemNum = record1ItemLoop.getAndIncrement();
-                        String item2 = record2.get(recordCurItemNum);
-                        try {
-                            if(!item1.equalsIgnoreCase(item2))
-                                System.out.println("Item mismatch found at Row number "+recordRowNumber+" and at the column index "+recordCurItemNum+ " ("+item1+" -> "+item2+")");
+            CSVRecord record2 = recordListNxt.get((int) recordRowNumber-1);
+            int record1ItemLoop = 0;
+            for (String item1 : record1) {
+                String item2 = record2.get(record1ItemLoop);
+                try {
+                    if (!item1.equalsIgnoreCase(item2))
+                        System.out.println("Item mismatch found at Row number " + recordRowNumber + " and at the column index " + record1ItemLoop + " (" + item1 + " -> " + item2 + ")");
 
-                        }catch (Exception e)
-                        {
-                            e.printStackTrace();
-                        }
-
-                });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                record1ItemLoop++;
+            }
 
 
-
-        });
+        }
 
     }
 
@@ -102,13 +106,13 @@ public class CSVOperator {
         CSVWriter writer = new CSVWriter();
         CSVReader reader = new CSVReader();
         CSVOperator operator = new CSVOperator();
-//        operator.compareTwoCSVFile("data//output//benin.csv","data//output//iraq.csv");
+        operator.compareTwoCSVFile("data//input//compare//csvSource.csv","data//input//compare//csvSource2.csv");
 //        operator.extractContentAndWrite("data//input//people.csv","data//output//benin.csv","Country","norway","Name","Age","Date of Birth","Country");
 
-        List<CSVRecord> recordList=CSVMatcher.getInstance("data//input//people.csv").getAllMatchingRecord().where().headerValueEquals("age",36)
-        .and().headerValueEquals("Country","Singapore").build();
-
-        CSVPrinter printer = writer.createCSVPrinter("data//output//benin2.csv","Name","Age","Phone","email","City","Country");
-        writer.writeCSVPrinter(reader.getFilteredOutColumnPrinter(recordList,printer,"Name","Age","Phone","email","City","Country"));
+//        List<CSVRecord> recordList=CSVMatcher.getInstance("data//input//people.csv").getAllMatchingRecord().where().headerValueEquals("age",36)
+//        .and().headerValueEquals("Country","Singapore").build();
+//
+//        CSVPrinter printer = writer.createCSVPrinter("data//output//benin2.csv","Name","Age","Phone","email","City","Country");
+//        writer.writeCSVPrinter(reader.getFilteredOutColumnPrinter(recordList,printer,"Name","Age","Phone","email","City","Country"));
     }
 }
